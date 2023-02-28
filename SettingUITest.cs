6231e2fa6b4bead2f133ffa5f95a78ef73740fe3
Shreya20112 <83803283+Shreya20112@users.noreply.github.com>
2023-02-28 17:57:45 +0530

    Add files via upload
    I want to check back button is displayed or not. After that it's navigating on previous page or not. It's not completed.

---
+++ SettingUITest.cs
@@ -0,0 +1,48 @@
+﻿using MudBlazor;
+using Ossur.Bionics.Common.Models.Peripherals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bunit;
+using Xunit;
+using Ossur.Bionics.Common.Utils;
+
+namespace BionicAppTestRunner.BionicAppUi
+{
+    public class SettingUITest
+    {
+        //[Fact]
+
+        // public void BackButton_AreDisplayed()
+        //{
+        //    var component = RenderComponent<Settings>();
+        //    var button = component.FindComponent<MudButton>();
+        //    button.Find("#BackButton").DoubleClick();
+        //    var text = component.FindComponent<MudText>();
+        //    Assert.Equal("1", text.Find("h6").TextContent);
+       
+        //}
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
