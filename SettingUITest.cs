a496038a67076446c472fdc0b29001857dc1cb5d
Shreya20112 <83803283+Shreya20112@users.noreply.github.com>
2023-03-01 18:09:52 +0530

    Update SettingUITest.cs
    Add two test cases

--- SettingUITest.cs
+++ SettingUITest.cs
@@ -1,41 +1,56 @@
-﻿using MudBlazor;
+﻿using BionicApp.Pages.Add_Device.My_Devices.DeviceSettings;
+using Bunit;
+using MudBlazor;
 using Ossur.Bionics.Common.Models.Peripherals;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Bunit;
 using Xunit;
-using Ossur.Bionics.Common.Utils;
 
 namespace BionicAppTestRunner.BionicAppUi
 {
-    public class SettingUITest
+    public class SettingsUITest : BionicAppUiTestBase
     {
-        //[Fact]
+        [Fact]
+        public void BackButton_AreDisplayed()
+        {
 
-        // public void BackButton_AreDisplayed()
-        //{
-        //    var component = RenderComponent<Settings>();
-        //    var button = component.FindComponent<MudButton>();
-        //    button.Find("#BackButton").DoubleClick();
-        //    var text = component.FindComponent<MudText>();
-        //    Assert.Equal("1", text.Find("h6").TextContent);
-       
-        //}
+            var component = RenderComponent<Settings>();
+            var button = component.FindComponent<MudButton>();
+            var BackButton = component.FindAll("button").FirstOrDefault(b => b.TextContent == "ArrowBack");
+            Assert.NotNull(BackButton);
+        }
 
+        [Fact]
+
+        public void Device_IsDisplayed() 
+        {
+            var component = RenderComponent<Settings>();
+            var peripheral = new Peripheral();
+            var text = component.FindComponent<MudText>();
+            var expectedDisplayName = component.FindAll("DisplayName").FirstOrDefault(b => b.TextContent == "MyDevice");
+            peripheral.SetValue("displayName", expectedDisplayName);
+            var actualDisplayName = peripheral.DisplayName;
+            Assert.Equal("expectedDisplayName", actualDisplayName);
+        }
+
+        [Fact]
+
+        public void OnRelaxModeClicked()
+        { 
+            var component = RenderComponent<Settings>();
+            var button = component.FindComponent<MudButton>();
+            button.Find("#OnRelaxModeClicked").DoubleClick();
+            var text = component.FindComponent<MudText>();
+            Assert.Null("RelaxMode");
+        }
     }
 
+    
+
 }
-
-
-
-
-
-
-
-
 
 
 
