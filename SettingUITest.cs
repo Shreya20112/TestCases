38e406a2b7630b75018f21f66f28d81d24185701
Shreya20112 <83803283+Shreya20112@users.noreply.github.com>
2023-03-02 18:04:12 +0530

    Update SettingUITest.cs
    Add three cases

--- SettingUITest.cs
+++ SettingUITest.cs
@@ -46,6 +46,43 @@ namespace BionicAppTestRunner.BionicAppUi
             var text = component.FindComponent<MudText>();
             Assert.Null("RelaxMode");
         }
+        
+         [Fact]
+
+        public void Vibration_feedbackIsShowing()
+        {
+            var component = RenderComponent<Settings>();
+            var text = component.FindComponent<MudText>();
+            Assert.Equal("Vibrationfeedback", text.Find("50%").TextContent);
+        }
+
+        [Fact]
+
+        public void Name1_Name2_TogglesAreDisplayed()
+        {
+            var component = RenderComponent<Settings>();
+            var text = component.FindComponent<MudText>();
+            Assert.Equal("name1", "name2");
+            Switch switchControl = new Switch { IsToggled = true };
+            var togglebutton = component.FindAll("button").FirstOrDefault(b => b.TextContent == "enabled");
+            var istoggleButtonDisabled = togglebutton?.HasAttribute("disabled") ?? false;
+
+            Assert.False(istoggleButtonDisabled);
+        }
+
+        [Fact]
+        
+        public void AllFieldsAndButtons_AreDisplayed()
+        {
+            var component = RenderComponent<Settings>();
+            var text = component.FindComponent<MudText>();
+            Assert.Equal("advancesettings", text.Find("h5").TextContent);
+            Assert.Equal("RampAdaption",text.Find("value").TextContent);
+            var value1 = component.FindAll("value1").FirstOrDefault(b => b.TextContent == "inclined_ua");
+            var value2 = component.FindAll("value2").FirstOrDefault(b => b.TextContent == "declined_ua");
+            Assert.Equal(value1.TextContent, value2.TextContent);
+        }
+
     }
 
     
